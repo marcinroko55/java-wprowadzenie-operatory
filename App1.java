c22ef1d1c4c2fe2a8e506c0c6d0b7b5f8e14c196
marcinroko55 <79092872+marcinroko55@users.noreply.github.com>
2021-07-01 18:35:46 +0200

    liczenie promienia okręgu

---
+++ App1.java
@@ -0,0 +1,11 @@
+class App1 {
+	public static void main (String args[]) {
+	
+	double pi = 3.14;
+	int radius = 7;
+	double result = 2*pi*radius;
+	System.out.println("Wynik wynosi: "+result);
+	
+	}
+
+}
